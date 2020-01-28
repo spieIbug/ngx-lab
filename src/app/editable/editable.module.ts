0342cb4863596a0c22128de681371cf5350c2f54
meyacine <my.meddah@gmail.com>
2020-01-28 21:37:37 +0100

    editable + simple routing

---
+++ src/app/editable/editable.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditableComponent } from './editable.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  declarations: [ EditableComponent ],
+  exports: [ EditableComponent ],
+})
+export class EditableModule {
+}
